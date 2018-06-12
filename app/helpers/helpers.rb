3fc07a0c8c11a5c0fd9ae3c1a669b580ba24be62
Yina Zhu <yinagit@gmail.com>
2018-06-12 16:47:29 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,11 +2,11 @@
 
 class Helpers
 
-  def current_user(sessions)
+  def self.current_user(sessions)
     @user = User.find_by_id(sessions[:user_id])
   end
 
-  def is_logged_in?(sessions)
+  def self.is_logged_in?(sessions)
     !!session[:user_id] #a double bang returns either true or false
   end
 end
